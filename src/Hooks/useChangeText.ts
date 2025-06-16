6aa206fa4afdbf7cfbdeeecf07d572409a652820
moshaosama <mohamedOSFekry@gmail.com>
2025-06-16 17:03:38 +0300

    added: custom Hooks folder

---
+++ src/Hooks/useChangeText.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useChangeText = () => {
+  const allTexts = ["Full-Stack Developer", "UI/UX Designer"];
+  const [startWord, setStartWord] = useState(allTexts[0]);
+  const [endWord, setEndWord] = useState(allTexts[1]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    let currentStartWordIndex = 1;
+    let currentEndWordIndex = 1;
+    const interval = setInterval(() => {
+      if (currentStartWordIndex <= startWord.length) {
+        setText(startWord.slice(0, currentStartWordIndex));
+        currentStartWordIndex++;
+      } else if (currentEndWordIndex <= endWord.length) {
+        setText(endWord.slice(0, currentEndWordIndex));
+        currentEndWordIndex++;
+      } else {
+        currentStartWordIndex = 1;
+        currentEndWordIndex = 1;
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return { text };
+};
+
+export default useChangeText;
---
+++ src/Hooks/useSetAnimate.ts
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const useSetAnimate = () => {
+  const [isAnimate, setIsAnimate] = useState(false);
+
+  useEffect(() => {
+    const handleAnimate = async () => {
+      await new Promise(() => setTimeout(() => setIsAnimate(!isAnimate), 700));
+    };
+
+    handleAnimate();
+  }, [isAnimate]);
+
+  return { isAnimate };
+};
+
+export default useSetAnimate;
