aa988363e6cf1704ec40e60bee485399e7984860
moshaosama <mohamedOSFekry@gmail.com>
2025-06-16 20:58:59 +0300

    modified: added new section links to header

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -6,6 +6,7 @@ import { IoCodeOutline, IoPersonOutline } from "react-icons/io5";
 import { BsBagCheck } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
 import { useEffect, useState } from "react";
+import { CgMenu } from "react-icons/cg";
 const Header = () => {
   const { container } = globalStyle;
   const [isScroll, setIsScroll] = useState(false);
@@ -36,12 +37,15 @@ const Header = () => {
         <div className="text-4xl font-bold bg-gradient-to-r from-blue-400 to-purple-400  bg-clip-text text-transparent">
           <h1 className="text-lg font-sans">Mohamed Osama</h1>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-8 max-sm:hidden max-xl:gap-4">
           <LinkHeader linkIcon={<CiHome />} linkTitle="Home" />
           <LinkHeader linkIcon={<IoPersonOutline />} linkTitle="About" />
           <LinkHeader linkIcon={<BsBagCheck />} linkTitle="Projects" />
           <LinkHeader linkIcon={<IoCodeOutline />} linkTitle="Skills" />
           <LinkHeader linkIcon={<MdOutlineEmail />} linkTitle="Contact" />
+        </div>
+        <div className="sm:hidden">
+          <CgMenu size={25} />
         </div>
       </header>
     </>
