0f6329bd3151b4b807f722f69c555841842ae99d
moshaosama <mohamedOSFekry@gmail.com>
2025-06-16 21:58:19 +0300

    modified: added animation triggers for Hero and Skills sections

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -6,9 +6,14 @@ import Projects from "../../Features/Projects/Projects";
 import Skills from "../../Features/Skills/Skills";
 import TouchMail from "../../Features/TouchMail/TouchMail";
 import TransitionProvider from "../../Context/TransitionContext";
+import { FaChevronUp } from "react-icons/fa";
+import useSetAnimate from "../../Hooks/useSetAnimate";
+import { cn } from "../../lib/cn";
+import SidebarProivder from "../../Context/SidebarContext";
 
 const Home = () => {
   const [scrollY, setScrollY] = useState(0);
+  const { isAnimate } = useSetAnimate();
 
   useEffect(() => {
     const handleScroll = () => setScrollY(window.scrollY);
@@ -19,7 +24,9 @@ const Home = () => {
   return (
     <>
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 animate-gradient-x">
-        <Header />
+        <SidebarProivder>
+          <Header />
+        </SidebarProivder>
         <TransitionProvider>
           <Hero ScrollY={scrollY} />
           <About />
@@ -28,6 +35,16 @@ const Home = () => {
         </TransitionProvider>
         <TouchMail />
       </div>
+      <a href="#">
+        <div
+          className={cn(
+            "fixed transition-all duration-500 right-10 cursor-pointer bg-white w-10 h-10 flex justify-center items-center rounded-full",
+            isAnimate ? "bottom-10" : "bottom-14"
+          )}
+        >
+          <FaChevronUp />
+        </div>
+      </a>
     </>
   );
 };
