f47d242dd76e6752cca90d7c92dfa7447338e48a
moshaosama <mohamedOSFekry@gmail.com>
2025-06-16 19:23:54 +0300

    added: shared data for skills and projects

---
+++ src/Data/ProjectData.ts
@@ -0,0 +1,82 @@
+export const ProjectData = [
+  {
+    image: "1734644116593.jpeg",
+    Title: "Spotify Clone",
+    Description:
+      "Full-stack Spotify clone with MEAN stack, featuring artist dashboard and integrated authentication.",
+    Skills: [
+      { name: "Angular" },
+      { name: "Node.js" },
+      { name: "MongoDB" },
+      { name: "Express" },
+    ],
+  },
+  {
+    image: "Screenshot (162).png",
+    Title: "Forever_Brand",
+    Description:
+      "Forever Brand is a modern e-commerce platform designed for clothing brands. It provides a seamless shopping experience with secure authentication, payment integration, and an admin dashboard for product management.",
+    Skills: [
+      { name: "React" },
+      { name: "Node.js" },
+      { name: "SQL" },
+      { name: "Express" },
+      { name: "Redux Toolkit" },
+    ],
+  },
+  {
+    image: "Screenshot (158).png",
+    Title: "BanKist",
+    Description:
+      "A responsive and animated frontend web app built entirely using React.js.",
+    Skills: [
+      { name: "React" },
+      { name: "React Hooks" },
+      { name: "Tailwind CSS" },
+      { name: "CSS Modules" },
+    ],
+  },
+  {
+    image: "Screenshot (164).png",
+    Title: "ZACHA BAZAR",
+    Description:
+      "A full-stack e-commerce application built with MERN stack (MongoDB, Express, React, Node.js). Features include secure payments, authentication, and deployment for a seamless shopping experience.",
+    Skills: [
+      { name: "React" },
+      { name: "Node.js" },
+      { name: "MongoDB" },
+      { name: "Express" },
+      { name: "JWT" },
+      { name: "Payment Gateway APIs" },
+    ],
+  },
+  {
+    image: "",
+    Title: "LinkedIn_Clone",
+    Description:
+      "LinkedIn Clone is a web application built with Angular and Spring Boot, designed to replicate key features of LinkedIn, allowing users to sign up, create posts, and interact with content.",
+    Skills: [
+      { name: "Angular" },
+      { name: "Tailwind CSS" },
+      { name: "Java" },
+      { name: "Spring Boot" },
+      { name: "Spring Security" },
+      { name: "PostgreSQL" },
+    ],
+  },
+
+  {
+    image: "",
+    Title: "Task-Management",
+    Description:
+      "A task management application that allows users to organize and track their tasks. The app includes features like user authentication, task CRUD operations, task categories, and LinkedIn profile scraping using Puppeteer.",
+    Skills: [
+      { name: "Next.js" },
+      { name: "TypeScript" },
+      { name: "Nest.js" },
+      { name: "MongoDB " },
+      { name: "Puppeteer" },
+      { name: "Tailwind CSS" },
+    ],
+  },
+];
---
+++ src/Data/Skills.ts
@@ -0,0 +1,42 @@
+export const SkillData = [
+  {
+    name: "javaScript",
+    color: "orange",
+    degree: "90%",
+  },
+  {
+    name: "React",
+    color: "blue",
+    degree: "85%",
+  },
+  {
+    name: "Node js",
+    color: "green",
+    degree: "80%",
+  },
+  {
+    name: "TypeScript",
+    color: "blue",
+    degree: "75%",
+  },
+  {
+    name: "Angular",
+    color: "red",
+    degree: "80%",
+  },
+  {
+    name: "MongoDB",
+    color: "green",
+    degree: "75%",
+  },
+  {
+    name: "CSS/Tailwind",
+    color: "yellow",
+    degree: "90%",
+  },
+  {
+    name: "Python",
+    color: "pink",
+    degree: "60%",
+  },
+];
